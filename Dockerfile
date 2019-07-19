d9440ab0d9e8e1640d27ce5b3e89580a84a0d87a
Joao Junior <jcajcefet@yahoo.com.br>
2019-07-19 23:35:26 +0200

    Update structure of the project

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /code
 ADD requirements.txt /code
 RUN pip install -r requirements.txt
 ADD src/api /code/src/api
+ADD scripts/db.sh /code/src/api
 ADD src/crawler /code/src/crawler
 ADD tests /code/tests
 ENV FLASK_APP api/app.py
--- src/api/app.py
+++ src/api/app.py
@@ -1,44 +1,25 @@
 from flask import Flask
-from flask import jsonify
-from flask_restful import Resource, Api
+from flask_restful import Api
 
-from crawler import crawlers
-from crawler import parsers
+from resources import ProcessResource
 
 
-app = Flask(__name__)
-api = Api(app)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config')
+    from models import db
+    from schemas import ma
+    ma.init_app(app)
+    db.init_app(app)
+    api = Api(app)
+    api.add_resource(ProcessResource, '/<string:process_number>')
+
+    return app
 
 
-class Process(Resource):
-    TJAL = '8.02'
-    TJMS = '8.12'
+app = create_app()
 
-    def get(self, process_number):
-        if len(process_number) != 25:
-            return {
-                'msg': f'Número do processo {process_number} inválido'}, 422
-        identify = process_number[-9:-5]
-        if identify != self.TJAL and identify != self.TJMS:
-            return {'msg':
-                    f'Processo {process_number} não pertence a TJAL ou TJMS'
-                    }, 422
-        elif identify == self.TJAL:
-            page1 = crawlers.get_page_from_first_instance_TJAL(process_number)
-            page2 = crawlers.get_page_from_second_instance_TJAL(process_number)
-            grade_1 = parsers.process(page1)
-            grade_2 = parsers.process(page2)
-        elif identify == self.TJMS:
-            page1 = crawlers.get_page_from_first_instance_TJMS(process_number)
-            page2 = crawlers.get_page_from_second_instance_TJMS(process_number)
-            grade_1 = parsers.process(page1)
-            grade_2 = parsers.process(page2)
-        return jsonify({'Número do processo': process_number,
-                        '1 Grau': grade_1,
-                        '2 Grau': grade_2})
-
-
-api.add_resource(Process, '/<string:process_number>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app = create_app()
+    app.run()
