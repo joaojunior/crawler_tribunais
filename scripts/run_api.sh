99da12d412ad3a56ce44cf14059b70a7512e9f43
Joao Junior <jcajcefet@yahoo.com.br>
2019-07-20 16:17:08 +0200

    Add celery to process the task async

--- scripts/run_api.sh
+++ scripts/run_api.sh
@@ -1,1 +1,1 @@
-env PYTHONPATH=src gunicorn -w 4 -b 0.0.0.0:4000 --chdir=src/api app:app
+env PYTHONPATH=src gunicorn -w 4 -b 0.0.0.0:4000 --chdir=src/api app:app_flask
--- src/api/resources.py
+++ src/api/resources.py
@@ -1,9 +1,8 @@
 from flask_restful import Resource
 
-from crawler import crawlers
-from crawler import parsers
 from models import db, Process
 from schemas import ProcessSchema
+from tasks import crawler_task
 
 
 class ProcessResource(Resource):
@@ -19,22 +18,13 @@ class ProcessResource(Resource):
             return {'msg':
                     f'Processo {process_number} não pertence a TJAL ou TJMS'
                     }, 422
-        process_schema = ProcessSchema()
         process = Process.query.get(process_number)
-        if process is not None:
-            return process_schema.jsonify(process)
-        if identify == self.TJAL:
-            page1 = crawlers.get_page_from_first_instance_TJAL(process_number)
-            page2 = crawlers.get_page_from_second_instance_TJAL(process_number)
-            grade_1 = parsers.process(page1)
-            grade_2 = parsers.process(page2)
-        elif identify == self.TJMS:
-            page1 = crawlers.get_page_from_first_instance_TJMS(process_number)
-            page2 = crawlers.get_page_from_second_instance_TJMS(process_number)
-            grade_1 = parsers.process(page1)
-            grade_2 = parsers.process(page2)
-        process = Process(number=process_number, grade1=grade_1,
-                          grade2=grade_2)
-        db.session.add(process)
-        db.session.commit()
+        if process is None:
+            process = Process(process_number=process_number)
+            db.session.add(process)
+            db.session.commit()
+            crawler_task.delay(process_number, 1)
+            crawler_task.delay(process_number, 2)
+
+        process_schema = ProcessSchema()
         return process_schema.jsonify(process)
