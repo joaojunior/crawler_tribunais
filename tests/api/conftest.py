1bc4fcc8e461a1d51d7500211104ad68ef2d992e
Joao Junior <jcajcefet@yahoo.com.br>
2019-07-20 16:29:12 +0200

    Change name app in the tests

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -5,7 +5,7 @@ from api import app
 
 @pytest.fixture
 def client():
-    app.app.config['TESTING'] = True
-    client = app.app.test_client()
+    app.app_flask.config['TESTING'] = True
+    client = app.app_flask.test_client()
 
     yield client
